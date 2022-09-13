b5d5a2ef012aac70b37490d746db1095af088a4d
MohdTazammul <tazammul1683@gmail.com>
2022-09-14 03:09:07 +0530

    converted DOM to React

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import TypingTest from './Components/TypingTest';
+
+function App() {
+  return (
+    <div className="App">
+     <TypingTest />
+    </div>
+  );
+}
+
+export default App;
