b95bf51817d4aed0228d0646c303343be533b80e
MohdTazammul <tazammul1683@gmail.com>
2022-09-17 03:40:30 +0530

    Created and updated components

---
+++ src/Components/StatsCard/StatsCard.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Icon } from "@iconify/react";
+
+import "./style.scss";
+function StatsCard({ minutes, seconds, accuracy, speed }) {
+  console.log(minutes);
+  return (
+    <div id="StatsCard">
+      <div>
+      <Icon className="icon" icon="fluent:timer-12-filled" color="#5bccf6" />
+        <span id="timer">
+          {minutes < 10 ? "0" + minutes : minutes}:
+          {seconds < 10 ? "0" + seconds : seconds}
+        </span>
+      </div>
+      <div>
+      <Icon className="icon" icon="material-symbols:speed-rounded" color="#5bccf6" />
+        <span id="speed">{isNaN(speed) ? 0 : speed}</span>
+      </div>
+      <div>
+      <Icon className="icon" icon="mingcute:aiming-2-fill" color="#5bccf6" /><span id="accuracy">{accuracy}%</span>
+      </div>
+    </div>
+  );
+}
+
+export default StatsCard;
