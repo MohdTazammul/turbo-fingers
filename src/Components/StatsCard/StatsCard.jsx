f634e20457dcb6d6f411d949bab1f7d988a8bf67
MohdTazammul <tazammul1683@gmail.com>
2022-09-18 20:25:07 +0530

    Made some modifications in statistics card

--- src/Components/StatsCard/StatsCard.jsx
+++ src/Components/StatsCard/StatsCard.jsx
@@ -3,22 +3,28 @@ import { Icon } from "@iconify/react";
 
 import "./style.scss";
 function StatsCard({ minutes, seconds, accuracy, speed }) {
-  console.log(minutes);
   return (
     <div id="StatsCard">
       <div>
-      <Icon className="icon" icon="fluent:timer-12-filled" color="#5bccf6" />
+        <Icon className="icon" icon="fluent:timer-12-filled" color="#5bccf6" />
         <span id="timer">
           {minutes < 10 ? "0" + minutes : minutes}:
           {seconds < 10 ? "0" + seconds : seconds}
         </span>
       </div>
+      <div className="separator"><span></span></div>
       <div>
-      <Icon className="icon" icon="material-symbols:speed-rounded" color="#5bccf6" />
+        <Icon
+          className="icon"
+          icon="material-symbols:speed-rounded"
+          color="#5bccf6"
+        />
         <span id="speed">{isNaN(speed) ? 0 : speed}</span>
       </div>
+      <div className="separator"><span></span></div>
       <div>
-      <Icon className="icon" icon="mingcute:aiming-2-fill" color="#5bccf6" /><span id="accuracy">{accuracy}%</span>
+        <Icon className="icon" icon="mingcute:aiming-2-fill" color="#5bccf6" />
+        <span id="accuracy">{accuracy}%</span>
       </div>
     </div>
   );
