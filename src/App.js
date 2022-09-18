0a91372d1cda52e5ce16b4746222830e1b040b75
MohdTazammul <tazammul1683@gmail.com>
2022-09-18 15:35:54 +0530

    Added route for Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import {Route,Routes} from 'react-router-dom'
+import Navbar from './Components/Navbar/Navbar';
 import Home from './pages/Home/Home';
 import TypingTest from './pages/TypingTest/TypingTest';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
     <Routes>
       <Route path='Typing-Test' element={<TypingTest />} />
       <Route path='/' element={<Home />} />
