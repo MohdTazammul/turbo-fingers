cb7f82995622774008d3017802482a783190c48d
MohdTazammul <tazammul1683@gmail.com>
2022-09-22 02:06:44 +0530

    Updated UI of Score Page with some detailed report of typing speed

--- src/Components/ScoreCard/ScoreCard.jsx
+++ src/Components/ScoreCard/ScoreCard.jsx
@@ -1,29 +1,83 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./style.scss"
 import GaugeChart from "react-gauge-chart";
 function ScoreCard({speed, accuracy, totalTime, seconds, minutes, paragraph, freqOfWrongChars}) {
-    console.log(freqOfWrongChars)
+  var words = paragraph.join("").split(" ").length;
+  var characters = paragraph.length;
+    var mistakes = 0
+    for(var key in freqOfWrongChars)
+    {
+      mistakes+=freqOfWrongChars[key];
+    }
+   
   return (
+    
     <div id='ScoreCard'>
-        <div>
-            <GaugeChart style={{ height: "150px"}}
-            id="gauge-chart"
-            textColor="white"
-            nrOfLevels={3}
-            arcsLength={[0.4, 0.6, 1]}
-            colors={["#EA4228", "#F5CD19","#5BE12C" ]}
-            percent={speed/100}
-            arcPadding={0.02}
-            animate={true}
-            text
-        />
-        </div>
-        <div>
-            <div>Speed {speed}</div>
-            <div>accuracy {accuracy}</div>
-            <div>time {totalTime}</div>
-            {/* <div>wrongChars {wrongChars}</div> */}
-            {/* <div>freqOfWrongChars ${JSON.stringify(freqOfWrongChars)}</div> */}
+      <div className='top-cont'>
+      {/* <h1>Tests can be improved</h1> */}
+      </div>
+        <div className='middle-cont'>
+            <div>
+              <GaugeChart style={{width:"100%"}}
+                    id="gauge-chart"
+                    textColor="black"
+                    nrOfLevels={4}
+                    arcsLength={[0.4, 0.4, 0.5, 0.7]}
+                    colors={["#EA4228", "#F5CD19","#5BE12C", "#2d6e16"]}
+                    percent={speed/100}
+                    arcPadding={0.02}
+                    needleColor={"grey"}
+                    formatTextValue	={(speed)=>speed+" w/pm"}
+                    needleBaseColor={"black"}
+                    animate={true}
+                    text
+                />
+            </div>
+            <div className='card-cont'>
+              <div className='card'>
+                <div>Time</div>
+                <div>
+                  <div className='time'></div>
+                  <div>{minutes < 10 ? "0" + minutes : minutes}:
+              {seconds < 10 ? "0" + seconds : seconds}</div>
+                </div>
+              </div>
+              <div className='card'>
+                <div>Accuracy</div>
+                  <div>
+                    <div className='accuracy'></div>
+                    <div>{accuracy}<span style={{fontSize:"20px"}}>(%)</span></div>
+                  </div>
+              </div>
+              <div className='card'>
+                <div>Speed</div>
+                  <div>
+                    <div className='speed'></div>
+                    <div>{speed}<span style={{fontSize:"20px"}}>(wpm)</span></div>
+                  </div>
+              </div>
+              <div className='card'>
+                <div>Words</div>
+                  <div>
+                    <div className='words'></div>
+                    <div>{words}</div>
+                  </div>
+              </div>
+              <div className='card'>
+                <div>Characters</div>
+                  <div>
+                    <div className='chars'></div>
+                    <div>{characters}</div>
+                  </div>
+              </div>
+              <div className='card'>
+                <div>Mistakes</div>
+                  <div>
+                    <div className='wrong-entries'></div>
+                    <div>{mistakes}</div>
+                  </div>
+              </div>
+            </div>
         </div>
     </div>
   )
