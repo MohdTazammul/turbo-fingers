49417e78f11c5325728a80fc23e951119218a867
MohdTazammul <tazammul1683@gmail.com>
2022-09-25 23:27:23 +0530

    Added route for Leaderboard page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import {Route,Routes} from 'react-router-dom'
+import Leaderboard from './Components/Leaderboard/Leaderboard';
 import Navbar from './Components/Navbar/Navbar';
 import Home from './pages/Home/Home';
 import TypingTest from './pages/TypingTest/TypingTest';
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
     <Routes>
       <Route path='Typing-Test' element={<TypingTest />} />
+      <Route path='Leaderboard' element={<Leaderboard />} />
       <Route path='/' element={<Home />} />
     </Routes>
     </div>
