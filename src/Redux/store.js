1e1bc6782ccc0843763d82f2c6d34e21236555c3
MohdTazammul <tazammul1683@gmail.com>
2022-10-10 19:00:52 +0530

    Integrated Redux Persist for persisting the redux store on refresh

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,7 +1,18 @@
 import {legacy_createStore, applyMiddleware} from 'redux'
 import {reducer} from './reducer'
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
+import thunk from 'redux-thunk';
 
 
-let store=legacy_createStore(reducer);
+const persistConfig = {
+    key: 'root',
+    storage,
+  }
+  
 
-export {store}
+const persistedReducer = persistReducer(persistConfig, reducer)
+
+let store=legacy_createStore(persistedReducer, applyMiddleware(thunk));
+const persistor = persistStore(store);
+export {store, persistor}
