644dfe66409c5e24cabb6971820e294de80f925c
MohdTazammul <tazammul1683@gmail.com>
2022-10-10 19:01:48 +0530

    Wrapped App with PersistGate

--- src/index.js
+++ src/index.js
@@ -6,13 +6,17 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 
 import { Provider } from "react-redux";
-import { store } from "./Redux/store";
+import { store, persistor } from "./Redux/store";
+
+import {PersistGate} from "redux-persist/es/integration/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </BrowserRouter>
 );
