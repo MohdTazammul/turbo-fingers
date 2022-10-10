bf64504032d1d112ea15916a11264550a8d44c95
MohdTazammul <tazammul1683@gmail.com>
2022-10-10 19:02:08 +0530

    Handling 404 route

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import {Route,Routes} from 'react-router-dom'
 import Leaderboard from './Components/Leaderboard/Leaderboard';
 import Navbar from './Components/Navbar/Navbar';
@@ -8,13 +7,14 @@ import TypingTest from './pages/TypingTest/TypingTest';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Navbar />
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='Typing-Test' element={<TypingTest />} />
       <Route path='Leaderboard' element={<Leaderboard />} />
       <Route path='profile' element={<Profile /> } />
+      <Route path='*' element={<Home />} />
     </Routes>
     </div>
   );
